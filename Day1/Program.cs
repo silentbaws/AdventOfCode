0a1aa49d660a5538440ca059c9481b1be1181016
silentbaws <silentbaws@hotmail.com>
2019-12-03 20:55:11 -0500

    Finished Day 1 with my input

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.IO;
+
+//Problem Set https://adventofcode.com/2019/day/1
 
 namespace Day1 {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			StreamReader reader = new StreamReader("Input.txt");
+
+			int totalFuel = 0;
+
+			string input = reader.ReadLine();
+			while(input != null){
+				int fuelRequired = (int)Math.Floor(float.Parse(input) / 3f) - 2;
+				while(fuelRequired > 0) {
+					totalFuel += fuelRequired;
+					fuelRequired = (int)Math.Floor((float)fuelRequired / 3f) - 2;
+				}
+				input = reader.ReadLine();
+			}
+
+			Console.WriteLine(totalFuel);
 		}
 	}
 }
