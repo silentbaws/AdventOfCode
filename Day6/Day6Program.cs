c2443a77fa788e4826b91ccbea2c17a590b3f855
silentbaws <silentbaws@hotmail.com>
2019-12-06 08:24:20 -0500

    Slightly improved Day 6 performance. Made day 6 output results instead of performance metrics

--- Day6/Day6Program.cs
+++ Day6/Day6Program.cs
@@ -22,70 +22,70 @@ namespace Day6 {
 
 			string[] orbits = programInput.Split("\n");
 
-			System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-			stopwatch.Start();
-			for (int c = 0; c < 100; c++) {
-				Dictionary<string, Orbit> orbitObjects = new Dictionary<string, Orbit>();
+			Dictionary<string, Orbit> orbitObjects = new Dictionary<string, Orbit>();
 
-				Orbit santaOrbit = null, youOrbit = null;
+			Orbit santaOrbit = null, youOrbit = null;
 
-				foreach (string orbit in orbits) {
-					string[] bodies = orbit.Trim().Split(")");
-					Orbit orbit1 = null, orbit2 = null;
-					orbitObjects.TryGetValue(bodies[0], out orbit1);
-					orbitObjects.TryGetValue(bodies[1], out orbit2);
+			foreach (string orbit in orbits) {
+				string[] bodies = orbit.Trim().Split(")");
+				Orbit orbit1 = null, orbit2 = null;
+				orbitObjects.TryGetValue(bodies[0], out orbit1);
+				orbitObjects.TryGetValue(bodies[1], out orbit2);
 
-					if (orbit1 == null) {
-						orbit1 = new Orbit(bodies[0]);
-						orbitObjects.Add(orbit1.name, orbit1);
-					}
-					if (orbit2 == null) {
-						orbit2 = new Orbit(bodies[1]);
-						orbitObjects.Add(orbit2.name, orbit2);
-					}
-
-					if (orbit2.name.Equals("YOU")) {
-						youOrbit = orbit2;
-					}else if (orbit2.name.Equals("SAN")) {
-						santaOrbit = orbit2;
-					}
-
-					orbit2.orbiting = orbit1;
+				if (orbit1 == null) {
+					orbit1 = new Orbit(bodies[0]);
+					orbitObjects.Add(orbit1.name, orbit1);
+				}
+				if (orbit2 == null) {
+					orbit2 = new Orbit(bodies[1]);
+					orbitObjects.Add(orbit2.name, orbit2);
 				}
 
-				int totalOrbits = 0;
-				foreach (KeyValuePair<string, Orbit> orbit in orbitObjects) {
-					Orbit parent = orbit.Value.orbiting;
-					while (parent != null) {
-						totalOrbits++;
-						parent = parent.orbiting;
-					}
+				if (orbit2.name.Equals("YOU")) {
+					youOrbit = orbit2;
+				}else if (orbit2.name.Equals("SAN")) {
+					santaOrbit = orbit2;
 				}
 
-				if (santaOrbit != null && youOrbit != null) {
-					List<int> steps = new List<int>();
-					Orbit youOrbiting = youOrbit.orbiting;
+				orbit2.orbiting = orbit1;
+			}
 
-					int youSteps = 0;
-					while (youOrbiting != null) {
-						Orbit santaOrbiting = santaOrbit.orbiting;
-						int santaSteps = 0;
-						while (santaOrbiting != null) {
-							if (santaOrbiting == youOrbiting) {
-								steps.Add(santaSteps + youSteps);
-							}
-							santaOrbiting = santaOrbiting.orbiting;
-							santaSteps++;
-						}
-						youOrbiting = youOrbiting.orbiting;
-						youSteps++;
-					}
+			int totalOrbits = 0;
+			foreach (KeyValuePair<string, Orbit> orbit in orbitObjects) {
+				Orbit parent = orbit.Value.orbiting;
+				while (parent != null) {
+					totalOrbits++;
+					parent = parent.orbiting;
 				}
 			}
-			stopwatch.Stop();
 
-			Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds/100);
-			
+			Console.WriteLine("Total Orbits: " + totalOrbits);
+
+			if (santaOrbit != null && youOrbit != null) {
+				int steps = 0;
+				Orbit youOrbiting = youOrbit.orbiting;
+
+				int youSteps = 0;
+				while (youOrbiting != null) {
+					Orbit santaOrbiting = santaOrbit.orbiting;
+					int santaSteps = 0;
+					while (santaOrbiting != null) {
+						if (santaOrbiting == youOrbiting) {
+							steps = santaSteps + youSteps;
+							break;
+						}
+						santaOrbiting = santaOrbiting.orbiting;
+						santaSteps++;
+					}
+					youOrbiting = youOrbiting.orbiting;
+					youSteps++;
+
+					if (steps != 0)
+						break;
+				}
+
+				Console.WriteLine("Minimum orbital transfers: " + steps);
+			}
 		}
 	}
 }
